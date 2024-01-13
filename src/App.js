3df5faed4dbbb055af62fdeb9d9fcf0867df344a
apodgornyitba <apodgorny@itba.edu.ar>
2024-01-13 19:55:45 -0300

    Feat: Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import {Card, TextField} from "@mui/material";
+import icon from './assets/images/icon-arrow.svg'
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [years, setYears] = useState('--')
+    const [months, setMonths] = useState('--')
+    const [days, setDays] = useState('--')
+
+    return (
+        <div className="App">
+            <Card className="primaryCard"
+                  sx={{
+                        width: '800px',
+                      '& .MuiInputBase-root': {
+                          fontFamily: 'inherit',
+                          fontWeight: 'bold',
+                          fontSize: '32px',
+                          marginRight: '25px',
+                      },
+                      '& .MuiOutlinedInput-input': {
+                          height: '50%',
+                          width: '115px',
+                      },
+                  }}>
+                <div className="date">
+                    <div className="day">
+                        <p>DAY</p>
+                    </div>
+                    <div className="month">
+                        <p>MONTH</p>
+                    </div>
+                    <div className="year">
+                        <p>YEAR</p>
+                    </div>
+                </div>
+                <div className="textEntry">
+                    <TextField
+                        placeholder='DD'
+                    />
+                    <TextField
+                        placeholder='MM'
+                    />
+                    <TextField
+                        placeholder='YYYY'
+                    />
+                </div>
+                <div className="lineContainer">
+                    <hr className="line"/>
+                    <div className="circle">
+                        <img src={icon} alt="icon"/>
+                    </div>
+                </div>
+                <div className="ageDisplay">
+                    <div className="years">
+                        <h1>{years} years</h1>
+                    </div>
+                    <div className="months">
+                        <h1>{months} months</h1>
+                    </div>
+                    <div className="days">
+                        <h1>{days} days</h1>
+                    </div>
+                </div>
+            </Card>
+        </div>
+    );
 }
 
 export default App;
